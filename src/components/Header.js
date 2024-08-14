7f11f35dfc73bb23ba637a33053ab1111a135ae5
heeyupchung <heeyupchung@gmail.com>
2024-08-14 14:09:39 -0700

    create new header and body, install tailwind and sass

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+export default function Header() {
+  return <div className="header">
+    <span>Pokemon Team Randomizer</span>
+    <span></span>
+  </div>
+}
---
+++ src/components/RandomizerPanel.js
@@ -0,0 +1,5 @@
+export default function RandomizerPanel() {
+  return <div>
+    Randomizer Panel
+  </div>
+}
