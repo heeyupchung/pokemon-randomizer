ebbc428d4b8751d918ef394f4bbdd1ba2908de85
heeyupchung <heeyupchung@gmail.com>
2024-10-08 18:23:26 -0700

    adds font awesome, adds placeholders to display pokemon

--- src/components/PokemonTeamDisplay.js
+++ src/components/PokemonTeamDisplay.js
@@ -1,26 +1,25 @@
 import { useState } from "react";
+import { pokemonPlaceholders } from "../variables/pokemonPlaceholders";
 
 export default function PokemonTeamDisplay({state, dispatch}) {
 
-  const [dummyData, setDummyData] = useState(
-      [
-        {name: "articuno"}, 
-        {name: "zapdos"}, 
-        {name: "moltres"}, 
-        {name: "mewtwo"}, 
-        {name: "dragonite"}, 
-        {name: "tyranitar"}
-      ]
-  );
-
   return <div className="pokemon-team-display">
     <div className="grid grid-cols-3 gap-4">
-      {state.team.map((pokemon) => {
+      {state.team.length > 0 ? state.team.map((pokemon) => {
         return <span key={pokemon.name} className="pokemon-card">
           <img src={pokemon.sprites.front_default} className="pokemon-sprites"/>
           {pokemon.name}
+          <span className="fas fa-edit"/>
         </span>
-      })}
+      }) : 
+        pokemonPlaceholders.map((pokemon, index) => {
+          return <div key={index} className="pokemon-card">
+          {/* <img src={pokemon.sprites.front_default} className="pokemon-sprites"/> */}
+          {pokemon}
+          <span className="fas fa-edit"/>
+          </div>
+        })
+      }
     </div>
   </div>
 }
---
+++ src/variables/pokemonPlaceholders.js
@@ -0,0 +1,8 @@
+export const pokemonPlaceholders = [
+  "Pokemon #1", 
+  "Pokemon #2", 
+  "Pokemon #3",
+  "Pokemon #4", 
+  "Pokemon #5",
+  "Pokemon #6"
+];
